2f56cc3b9187e3a8c3b81f5c4fe7e55a9e0ac3fc
hacktifytechnologies <hacktifytechnologies@gmail.com>
2026-03-30 00:46:20 +0530

    Add files via upload

---
+++ configs/roundcube_config.inc.php
@@ -0,0 +1,17 @@
+<?php
+$config['db_dsnw'] = 'mysql://roundcube:roundcube123!@localhost/roundcubedb';
+$config['default_host'] = 'localhost';
+$config['default_port'] = 143;
+$config['imap_host'] = 'localhost';
+$config['smtp_server'] = '';
+$config['smtp_port'] = 25;
+$config['smtp_user'] = '';
+$config['smtp_pass'] = '';
+$config['support_url'] = '';
+$config['product_name'] = 'CorpMail';
+$config['des_key'] = 'rcmail-!24ByteDESkey*Secure';
+$config['plugins'] = array();
+$config['skin'] = 'larry';
+$config['enable_installer'] = false;
+$config['log_driver'] = 'file';
+$config['log_dir'] = 'logs/';
